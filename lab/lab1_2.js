3da786693ce43b050a5240d09e98482df95b6040
kucharskiprzemek <kucharskiprzemyslawpiotr@gmail.com>
2015-10-19 11:08:13 +0200

    8 commit

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -8,7 +8,7 @@ var task =  function(request, callback){
 	callback( null, "czy to dziala?")
 	ec2.describeInstances(null, function(err, data) {
 	  if (err) console.log(err, err.stack); // an error occurred
-	  else     callback(null, data));           // successful response
+	  else     callback(null, data);           // successful response
 	});
 
 }
