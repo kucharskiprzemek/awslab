7424e95d8a976da8c201d9b511a94c514aec92cf
kucharskiprzemek <kucharskiprzemyslawpiotr@gmail.com>
2015-10-19 11:14:25 +0200

    10

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -1,8 +1,7 @@
 //stub for lab 1_2
 var AWS = require('aws-sdk');
-var ec2 = new AWS.EC2();
 AWS.config.loadFromPath('./config.json');
-
+var ec2 = new AWS.EC2();
 var task =  function(request, callback){
 
 	callback( null, "czy to dziala?")
