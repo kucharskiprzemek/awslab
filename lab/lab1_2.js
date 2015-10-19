98f617ad496575f13f75f1230fc713a3880b8d60
kucharskiprzemek <kucharskiprzemyslawpiotr@gmail.com>
2015-10-19 11:41:52 +0200

    12

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -2,11 +2,12 @@
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 var ec2 = new AWS.EC2();
+AWS.config.update({"region":"us-west-2"});
 var task =  function(request, callback){
 
 	callback( null, "czy to dziala?")
 	params = {}
-	ec2.describeInstances(params, function(err, data) {
+	ec2.describeInstances(null, function(err, data) {
 	  if (err) console.log(err, err.stack); // an error occurred
 	  else     callback(null, "SUKCES!");           // successful response
 	});
