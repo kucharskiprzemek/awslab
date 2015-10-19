e391f5d375d640561b13ddfe3d7b6ba272fa4fd4
kucharskiprzemek <kucharskiprzemyslawpiotr@gmail.com>
2015-10-19 11:26:52 +0200

    11

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -5,7 +5,8 @@ var ec2 = new AWS.EC2();
 var task =  function(request, callback){
 
 	callback( null, "czy to dziala?")
-	ec2.describeInstances(null, function(err, data) {
+	params = {}
+	ec2.describeInstances(params, function(err, data) {
 	  if (err) console.log(err, err.stack); // an error occurred
 	  else     callback(null, "SUKCES!");           // successful response
 	});
