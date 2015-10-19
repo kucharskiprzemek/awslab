44dd2f08f8bfc41ca5d300750c91ff6367f649a7
kucharskiprzemek <kucharskiprzemyslawpiotr@gmail.com>
2015-10-19 11:01:42 +0200

    5 commit

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -6,6 +6,11 @@ AWS.config.loadFromPath('./config.json');
 var task =  function(request, callback){
 
 	callback( null, "czy to dziala?")
+	ec2.describeInstances(null, function(err, data) {
+	  if (err) console.log(err, err.stack); // an error occurred
+	  else     console.log(data);           // successful response
+	});
+	callback(null, data)
 }
 
 exports.lab = task
